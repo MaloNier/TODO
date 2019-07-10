1b64f65ac6a54eb651fe088ee77ade04a12ba274
MaloNier <Hisamaru020@yahoo.co.jp>
2019-07-10 15:25:01 +0900

    room.htmlの追加と、それに伴う周辺の設定の整理

--- TODOapps/urls.py
+++ TODOapps/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index_func, general_signup_func, login_func, home_func, logout_func, good_func, RoomCreate
+from .views import index_func, general_signup_func, login_func, home_func, logout_func, good_func, RoomCreate, room_func
 
 urlpatterns = [
 	path('index/', index_func, name='index'),
@@ -8,5 +8,6 @@ urlpatterns = [
 	path('home/', home_func, name='home'),
 	path('logout/', logout_func, name='logout'),
 	path('good/<int:pk>', good_func, name='good'),
-	path('room_create/', RoomCreate.as_view(), name='room_create')
+	path('room_create/', RoomCreate.as_view(), name='room_create'),
+	path('room/<int:pk>', room_func, name='room'),
 ]
--- TODOapps/views.py
+++ TODOapps/views.py
@@ -58,12 +58,12 @@ def good_func(request, pk):
 	chat = ChatModel.objects.get(pk=pk)
 	good_user = request.user.get_username()
 	if good_user in chat.good_users:
-		return redirect('room/' + pk)
+		return redirect('room')
 	else:
 		chat.good += 1
 		chat.good_users = chat.good_users + ',' + good_user
 		chat.save()
-		return redirect('room/' + pk)
+		return redirect('room')
 
 ### ルーム作成
 class RoomCreate(CreateView):
@@ -71,3 +71,8 @@ class RoomCreate(CreateView):
 	model = RoomModel
 	field = {'title', 'author', 'images', 'positinos'}
 	success_url = reverse_lazy('home')
+
+@login_required
+def room_func(request, pk):
+	room = RoomModel.objects.get(pk=pk)
+	return(request, 'room.html', {'object': object})
